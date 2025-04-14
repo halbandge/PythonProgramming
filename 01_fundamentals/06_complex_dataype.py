d8ffb174bd6036c3ff2da831f52e077f4b31ae83
soham-halbandge <logzii123@gmail.com>
2025-04-15 01:02:26 +0530

    commit

--- 01_fundamentals/06_complex_dataype.py
+++
@@ -1,9 +0,0 @@
-x = 10 + 20j
-print(x) # (10+20j)
-print(type(x)) # <class 'complex'>
-
-# to get the real value from complex number
-print(x.real) # 10.0
-
-# to get the imaginary value from complex number
-print(x.imag) # 20.0
